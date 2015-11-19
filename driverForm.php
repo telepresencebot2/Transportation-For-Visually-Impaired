1c0608a20d735242fb33a111d0c3197e4351cc89
tpe0002 <tpe0002@auburn.edu>
2015-11-19 17:05:18 -0600

    some more driver stuff

--- driverForm.php
+++ driverForm.php
@@ -1,7 +1,32 @@
+<?php
+	function addVehicles(){
+		define("HOST", "localhost");
+		define("DATABASE", "db1");
+		// magical
+		define("U_R", "transMAGIC");
+		define("P_R", "bFYRFWc2jupQ9xbK");
+		$dbMAGIC = new PDO('mysql:host=localhost;dbname=db1', U_R, P_R);
+		insert = dbMAGIC->prepare('INSERT INTO vehicleColor(vehicleType, seats, color) VALUES
+		(:vehicleType, :vehicleSeats, :vehicleColor'));
+		$insert->bindParam(':vehicleType', $_POST['vehicleType']);
+		$insert->bindParam(':vehicleSeats', $_POST['vehicleSeats']);
+		$insert->bindParam(':vehicleColor', $_POST['vehicleColor']);
+		$insert->execute();
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <script src="jscolor-2.0.4/jscolor.js"></script>
+<script>
+	function clicked(){
+		if (confirm('Are your sure?')){
+			var phpadd = <?php echo addVehicles();?>;
+		}
+	}	
+</script>
+
 <style>
 	h2{
 		position: relative;
@@ -78,14 +103,17 @@
 	<form name = "driverForm" action="driverForm.php" method="post">
 		<div id="driverInfo">
 			<label for="vehicleType" id="firstLabel">Vehicle ID:</label>	
-			<input type="text" name="emergencyNumber"  id="firstField">
+			<input type="text" name="vehicleType"  id="firstField">
 			<p></p>
-			<label for="vehicleType" id="firstLabel">Vehicle Color:</label>	
-			<input class="jscolor jscolor-active" id="firstField"value="ab2567" autocomplete="off" style="color: 
+			<label for="vehicleSeats" id="firstLabel">Seats:</label>	
+			<input type="text" name="vehicleSeats"  id="firstField">
+			<p></p>
+			<label for="vehicleColor" id="firstLabel">Vehicle Color:</label>	
+			<input name="vehicleColor" class="jscolor jscolor-active" id="firstField"value="ab2567" autocomplete="off" style="color: 
 			rgb(255, 255, 255); background-image: none; background-color: rgb(171, 37, 103);">		
 		</div>
 		<div id="driverSave">
-			<button class="button" type="button">Save</button>
+			<button class="button" onclick="save();" type="button">Save</button>
 		</div>
 	</form>
 </body>
